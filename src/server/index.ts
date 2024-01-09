448daaf0305187a50684fef67de439c22bd72aea
javi <javier.bravoz@mail.udp.cl>
2024-01-09 17:12:59 -0300

    todos los componentes del trpc borrados

--- src/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { marzoRouter } from "./routers/marzo";
-import { publicProcedure, router } from "./trpc";
-
-export const appRouter = router({
-  greeting: publicProcedure.query(() => "Hello from tRPC"),
-  marzo: marzoRouter,
-});
-
-export type AppRouter = typeof appRouter;
