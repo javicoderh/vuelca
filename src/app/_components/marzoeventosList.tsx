20fbff4e7409ff7f70d0cc74af4d5c6ab3b69f7f
javi <javier.bravoz@mail.udp.cl>
2024-01-09 17:29:39 -0300

    archivos trpc agregados temporalmente

---
+++ src/app/_components/marzoeventosList.tsx
@@ -0,0 +1,85 @@
+/*"use client";
+
+import { useForm } from "react-hook-form";
+import { Marzo, defaultMarzoValues } from "@/lib/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MarzoEventosSchema, UserSchema } from "@/lib/models";
+import { trpc } from "../_trpc/client";
+
+
+
+export const MarzoEventosList = () => {
+  const { register, handleSubmit, reset } = useForm<Marzo>({
+    defaultValues: defaultMarzoValues,
+    resolver: zodResolver(MarzoEventosSchema),
+  });
+
+  const eventos = trpc.marzo.readAll.useQuery();
+  const crearEvento = trpc.marzo.create.useMutation({
+    onSuccess: () => {
+      eventos.refetch();
+      reset();
+    },
+  });
+  const updateMarzo = trpc.marzo.update.useMutation({
+    onSuccess: () => {
+      eventos.refetch();
+      reset();
+    },
+  });
+  const deleteMarzo = trpc.marzo.delete.useMutation({
+    onSuccess: () => {
+      eventos.refetch();
+    },
+  });
+
+  const onSubmit = (data: Marzo) => {
+    if (data.id) {
+      crearEvento.mutate(data);
+    } else {
+      crearEvento.mutate(data);
+    }
+  }; 
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("id")} />
+        <input type="text" {...register("nombre")} />
+        <input type="text" {...register("fecha")} />
+        <input type="text" {...register("descripcion")} />
+        <input type="text" {...register("categoria")} />
+        <input type="text" {...register("contacto")} />
+        <input type="text" {...register("ruta")} />
+        <input type="text" {...register("imagen1")} />
+        <input type="text" {...register("eslogan")} />
+        <input type="text" {...register("mes")} />
+        
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <ul>
+        {eventos.isLoading ? (
+          <li>Loading...</li>
+        ) : (
+          eventos.data?.map((evento) => (
+            <li key={evento.id}>
+              <span>{evento.nombre}</span>|{"nombre"}
+              <button
+                onClick={() =>
+                  reset(evento, {
+                    keepDefaultValues: true,
+                  })
+                }
+              >
+                Edit
+              </button>
+              | <button onClick={() => deleteMarzo.mutate(evento)}>Delete</button>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};*/
---
+++ src/server/routers/marzo.ts
@@ -0,0 +1,64 @@
+import { MarzoEventosSchema } from "@/lib/models";
+import { publicProcedure, router } from "../trpc";
+import { prisma } from "@/lib/prisma";
+import { TRPCError } from "@trpc/server";
+
+export const marzoRouter = router({
+  create: publicProcedure
+    .input(
+      MarzoEventosSchema.omit({
+        id: true,
+      })
+    )
+    .mutation(async (opts) => {
+      try {
+        return await prisma.marzoeventos.create({
+          data: opts.input,
+        });
+      } catch (error) {
+        new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Internal Server Error",
+        });
+      }
+    }),
+  readAll: publicProcedure.query(async () => {
+    try {
+      return await prisma.marzoeventos.findMany();
+    } catch (error) {
+      new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal Server Error",
+      });
+    }
+  }),
+  update: publicProcedure.input(MarzoEventosSchema).mutation(async (opts) => {
+    try {
+      return await prisma.marzoeventos.update({
+        where: {
+          id: opts.input.id,
+        },
+        data: opts.input,
+      });
+    } catch (error) {
+      new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal Server Error",
+      });
+    }
+  }),
+  delete: publicProcedure.input(MarzoEventosSchema).mutation(async (opts) => {
+    try {
+      return await prisma.marzoeventos.delete({
+        where: {
+          id: opts.input.id,
+        },
+      });
+    } catch (error) {
+      new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal Server Error",
+      });
+    }
+  }),
+});
