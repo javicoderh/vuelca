c65b1cedb68aeb54aaaeb7265acad1daa206371c
Jefferson <thesteelninjacode@gmail.com>
2024-01-10 13:57:50 -0600

    Remove unnecessary line in index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,6 @@
 import { marzoRouter } from "./routers/marzo";
 import { publicProcedure, router } from "./trpc";
 
-
 export const appRouter = router({
   greeting: publicProcedure.query(() => "Hello from tRPC"),
   marzo: marzoRouter,
