56c7ba9af3f5b04f04ef3877cdaaab4036652620
javi <javier.bravoz@mail.udp.cl>
2024-01-30 11:32:34 -0300

    r

--- src/app/ui/destacados-mob.tsx
+++ src/app/ui/destacados-mob.tsx
@@ -6,8 +6,7 @@ import CarouselDestacadosMobile from './destacados-mobile';
 
 const DestacadosMobile = () => {
   return (
-    <div className="w-full md:hidden md:flex flex-col h-[30vh] items-center justify-center destacados2 w-full">
-        <h1 className="destacados-title2 font-bold text-white mb-3">Emprendimientos destacados</h1>        
+    <div className="w-full md:hidden md:flex flex-col h-[30vh] items-center justify-center destacados2 w-full">        
         <CarouselDestacadosMobile />     
     </div>
   );
